7869879ba1aa6e1670244161f5742f6b26b87b37
HarshiniThulasi <138754345+HarshiniThulasi@users.noreply.github.com>
2023-07-08 22:00:13 +0530

    Add files via upload

---
+++ sql project.sql
@@ -0,0 +1,8 @@
+create database harshini;
+use harshini;
+create table Login(Usernsme varchar(30), password varchar(20));
+select * from Login;
+create table Product(ProductId int, productName varchar(20), minShellQuantity int, price int, quantity int);
+insert into Login values('harshini','harsh');
+desc product;
+select * from product;
